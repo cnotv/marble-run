f80506cef5d02e326006db199372e780a10b42da
cnotv <giuseppe.leo@suse.com>
2023-12-24 01:45:19 +0100

    Correct build path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 // vite.config.js
 module.exports = {
   root: './public', // the root directory for your project
+  build: {
+    outDir: '../dist', // the output directory for the build
+  },
   server: {
     port: 3000, // the port to run your dev server on
   },
